4e8046f092eac6aeef306d5a65457e90d682e4c9
cosre7 <76677629+cosre7@users.noreply.github.com>
2021-09-04 21:29:38 +0900

    Update MultiplicationUI.js

--- javaScript/multiplication/MultiplicationUI.js
+++ javaScript/multiplication/MultiplicationUI.js
@@ -1,17 +1,21 @@
 var MultiplicationUI = {};
 
 /*
-    곱셈 테이블을 출력한다.
+    이름 : printMultiplicationTable
+    역할 : 곱셈 테이블을 출력한다.
 */
 
 /*
-    곱셈 테이블을 컨트롤러에게 받는다.
+    이름 : getMultiplicationTable
+    역할 : 곱셈 테이블을 컨트롤러에게 받는다.
+*/
+
+/* 
+    이름 : giveInputValue
+    역할 : 인풋값을 컨트롤러에 준다.
 */
 
 /*
-    인풋값을 컨트롤러에 준다.
-*/
-
-/*
-    인풋값을 입력받는다.
+    이름 : getInputValue
+    역할 : 인풋값을 입력받는다.
 */
