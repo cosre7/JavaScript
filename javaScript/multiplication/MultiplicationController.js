8198f615ebf768e32340ae825f4f9ab496cf0360
cosre7 <76677629+cosre7@users.noreply.github.com>
2021-09-04 21:34:35 +0900

    Update MultiplicationController.js

--- javaScript/multiplication/MultiplicationController.js
+++ javaScript/multiplication/MultiplicationController.js
@@ -1,21 +1,21 @@
 var MultiplicationController = {};
 
 /*
-    뷰에게 테이블을 준다
-
+    이름 : giveViewMultiplicationTable
+    역할 : 뷰에게 테이블을 준다
 */
 
 /*
-    모델에게 테이블을 받는다
-
+    이름 : getModelMultiplicationTable
+    역할 : 모델에게 테이블을 받는다
 */
 
 /*
-    모델에게 인풋값을 준다
-
+    이름 : giveModelInputValue
+    역할 : 모델에게 인풋값을 준다
 */
 
 /*
-    뷰에게 인풋값을 받는다
-
+    이름 : getViewInputValue
+    역할 : 뷰에게 인풋값을 받는다
 */
