c040686bc13b38c9d67388823f11da3c734692c9
cosre7 <76677629+cosre7@users.noreply.github.com>
2021-09-05 22:16:23 +0900

    Update MultiplicationController.js

--- javaScript/multiplication/MultiplicationController.js
+++ javaScript/multiplication/MultiplicationController.js
@@ -1,21 +1,28 @@
 var MultiplicationController = {};
 
+// var model = MultiplicationFactory;
+// var view = MultiplicationUI;
 /*
-    이름 : giveViewMultiplicationTable
-    역할 : 뷰에게 테이블을 준다
+    인풋값을 모델에게 주고 테이블을 얻는다.
 */
+MultiplicationController.getTable = function(startRowNumber, endRowNumber, endColumNumber) {
+    var startColumNumber = 1;
+    var startRowNumber = document.getElementById("startRowNumber").value;
+    var endRowNumber = document.getElementById("endRowNumber").value;
+    var endColumNumber = document.getElementById("endColumNumber").value;
+    var createTable = MultiplicationFactory.createMultiplyTable(startColumNumber , endColumNumber , startRowNumber , endRowNumber);
+    MultiplicationUI.printMultiplicationTable(createTable,startRowNumber, endRowNumber, endColumNumber);
+    // console.log(MultiplicationController.giveTable(createTable));
+}
 
-/*
-    이름 : getModelMultiplicationTable
-    역할 : 모델에게 테이블을 받는다
-*/
-
-/*
-    이름 : giveModelInputValue
-    역할 : 모델에게 인풋값을 준다
-*/
-
-/*
-    이름 : getViewInputValue
-    역할 : 뷰에게 인풋값을 받는다
-*/
+// /*
+//     테이블을 뷰에게 준다.
+// */
+// MultiplicationController.giveTable = function(createTable,startRowNumber, endRowNumber, endColumNumber) {
+//     // MultiplicationUI = {'printMultiplicationTable' : createTable};
+//     console.log("성공");
+//     // console.log(createTable);
+//     var table = createTable;
+//     MultiplicationUI.printMultiplicationTable(table,startRowNumber, endRowNumber, endColumNumber);
+//     // return table
+// }
