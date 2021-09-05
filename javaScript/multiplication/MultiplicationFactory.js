98d998cb2ae0d19967ce2677fac409685b6c5d3f
cosre7 <76677629+cosre7@users.noreply.github.com>
2021-09-05 22:16:41 +0900

    Update MultiplicationFactory.js

--- javaScript/multiplication/MultiplicationFactory.js
+++ javaScript/multiplication/MultiplicationFactory.js
@@ -1,12 +1,10 @@
-var MultiplicationFactory = { row };
+var MultiplicationFactory = {};
 
 /*
 주석 : 
 이름 : createMultiplyTable
 역할 : 곱셈 테이블 (예를 들어 구구단 같은 표를 만든다.) 
-
 재료 : 
-
                                 0 (startColumNumber)     1 2 3 4 .............................. n (endColumNumber)
         
 0 (startRowNumber)              0 X 0 = 0     0 * 1 = 0   0 * 2 = 0 ........................... 0 * n = 0  
@@ -18,26 +16,23 @@ var MultiplicationFactory = { row };
 .
 m (endRowNumber)
 */
-var row = [];
 
+var multiplyTable = [];
 MultiplicationFactory.createMultiplyTable = function( startColumNumber , endColumNumber , startRowNumber , endRowNumber ) {
-
+    
     var count = startRowNumber;
-    var multiplyTable = [];
     while (count <= endRowNumber) {
         multiplyTable[count + "단"] = MultiplicationFactory.createMultiplyRow(count, startColumNumber, endColumNumber);
         ++count;
     }
-    MultiplicationFactory.row = multiplyTable;
-    // return multiplyTable;
+    document.write(multiplyTable);
+    return multiplyTable;
 }
 
 /*
 주석 : 
 이름 : createMultiplyRow
 역할 : 곱셈 테이블의 행을 만든다. (구구단에 한단에 해당 된다.)
-
-
 */
 MultiplicationFactory.createMultiplyRow = function( rowNumber , startColumNumber , endColumNumber) {
 
@@ -54,15 +49,15 @@ MultiplicationFactory.createMultiplyRow = function( rowNumber , startColumNumber
 주석 : 
 이름 : createMultiplicationOperation
 역할 : 곱셈 연산식을 만든다.
-
 */
 MultiplicationFactory.createMultiplicationOperation = function( rowNumber , columNumber ) {
     var result = rowNumber * columNumber;
-    var operation = rowNumber + " * " + columNumber + " = " + result;
-    var color = (rowNumber % 2 == 1) ? "blue" : "orange";  
-    var multiplicationOperation = {};
-    multiplicationOperation = {operation, color};
-    return multiplicationOperation
+    var operation = rowNumber + " * " + columNumber + " = " + result + "<br/>";
+    rowNumber % 2 == 1 ? operation.fontcolor("blue") : operation.fontcolor("orange");  
+    // var multiplicationOperation = {};
+    // multiplicationOperation = {operation, color};
+    // return multiplicationOperation
+    return operation;
 }
 
 /*
@@ -70,6 +65,9 @@ MultiplicationFactory.createMultiplicationOperation = function( rowNumber , colu
     역할 : multiplicationTable에서 한 단을 찾는다.
 */
 MultiplicationFactory.findMultiplyRow = function( rowNumber ) {
-    var setRow = rowNumber + "단";
-    return MultiplicationFactory.row[setRow];
+    for(key in multiplyTable) {
+        if(key == (rowNumber + "단")) {
+            return [rowNumber + "단", multiplyTable[rowNumber + "단"]]
+        }
+    }
 }
