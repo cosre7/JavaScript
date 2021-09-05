df06934884e1223183795450587d984444fff3c1
cosre7 <76677629+cosre7@users.noreply.github.com>
2021-09-05 22:26:35 +0900

    Update MultiplicationUI.js

--- javaScript/multiplication/MultiplicationUI.js
+++ javaScript/multiplication/MultiplicationUI.js
@@ -10,11 +10,13 @@ MultiplicationUI.printMultiplicationTable = function(table,startRowNumber, endRo
     for (i = startRowNumber; i <= endRowNumber; i++) {
         document.write("<tr>");
         for (j = startRowNumber; j <= i; j++) {
-            document.write(j + "단" + "<br/>", resultTable[j + "단"].join(""));
+            var color = (j % 2 == 1) ? "blue" : "orange";
+            document.write(j + "단 : " + color + "<br/>", resultTable[j + "단"].join(""));
         }
         document.write("</tr>");
         document.write("------------------<br/>")
     }
     document.write("</table>");
+    return resultTable;
 }
 
