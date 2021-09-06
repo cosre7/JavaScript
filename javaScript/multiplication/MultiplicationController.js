3baaa02132ecce2d35ac06b3f78918e09fa83354
cosre7 <76677629+cosre7@users.noreply.github.com>
2021-09-06 10:06:47 +0900

    start단이 end단보다 클 때 처리

--- javaScript/multiplication/MultiplicationController.js
+++ javaScript/multiplication/MultiplicationController.js
@@ -1,7 +1,5 @@
 var MultiplicationController = {};
 
-// var model = MultiplicationFactory;
-// var view = MultiplicationUI;
 /*
     인풋값을 모델에게 주고 테이블을 얻는다.
 */
@@ -10,19 +8,11 @@ MultiplicationController.getTable = function(startRowNumber, endRowNumber, endCo
     var startRowNumber = document.getElementById("startRowNumber").value;
     var endRowNumber = document.getElementById("endRowNumber").value;
     var endColumNumber = document.getElementById("endColumNumber").value;
-    var createTable = MultiplicationFactory.createMultiplyTable(startColumNumber , endColumNumber , startRowNumber , endRowNumber);
-    MultiplicationUI.printMultiplicationTable(createTable,startRowNumber, endRowNumber, endColumNumber);
-    // console.log(MultiplicationController.giveTable(createTable));
+    if (startRowNumber > endRowNumber) {
+        alert("첫 번째 단이 마지막 단보다 더 작아야 합니다.");
+        history.go(0);
+    } else {
+        var createTable = MultiplicationFactory.createMultiplyTable(startColumNumber , endColumNumber , startRowNumber , endRowNumber);
+        MultiplicationUI.printMultiplicationTable(createTable,startRowNumber, endRowNumber, endColumNumber);
+    }
 }
-
-// /*
-//     테이블을 뷰에게 준다.
-// */
-// MultiplicationController.giveTable = function(createTable,startRowNumber, endRowNumber, endColumNumber) {
-//     // MultiplicationUI = {'printMultiplicationTable' : createTable};
-//     console.log("성공");
-//     // console.log(createTable);
-//     var table = createTable;
-//     MultiplicationUI.printMultiplicationTable(table,startRowNumber, endRowNumber, endColumNumber);
-//     // return table
-// }
