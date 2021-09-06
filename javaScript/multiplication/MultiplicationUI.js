7a2078ab6994c88676de6e1bac966d795b29b8e5
cosre7 <76677629+cosre7@users.noreply.github.com>
2021-09-06 10:10:37 +0900

    구구단 세로방식으로 테이블 한 칸 안에 출력

--- javaScript/multiplication/MultiplicationUI.js
+++ javaScript/multiplication/MultiplicationUI.js
@@ -9,30 +9,13 @@ MultiplicationUI.printMultiplicationTable = function(table,startRowNumber, endRo
     document.write('<table border="1">');
 
     for (i = startRowNumber; i <= endRowNumber; i++) {
-        // 행별 최대 출력 갯수
-        var maxCount = endRowNumber - startRowNumber + 1 + 1;
-
         document.write("<tr>");
-        for (j = startRowNumber; j <= maxCount; j++) {
-            var color = (i % 2 == 1) ? "blue" : "orange";
-            
-            if(i > j) {
-                document.write("<th><font color=" + color + ">"  + "</th>");
-                document.write("<td><font color=" + color + ">"  + "</td>")
-            } else {
-                document.write("<th><font color=" + color + ">" + i + "단" + "</th>");
-                document.write("<td><font color=" + color + ">" + resultTable[i + "단"].join("") + "</td>")
-            }
+        for (j = startRowNumber; j <= i; j++) {
+            var color = (j % 2 == 1) ? "blue" : "orange";
+            document.write("<td><font color=" + color + ">" + j + "단 <br/>");
+            document.write("<font color=" + color + ">" + resultTable[j + "단"].join("") + "</td>")
         }
         document.write("</tr>");
-
-        // document.write("<tr>");
-        // for (j = startRowNumber; j <= i; j++) {
-        //     var color = (j % 2 == 1) ? "blue" : "orange";
-        //     document.write("<th><font color=" + color + ">" + j + "단" + "</th>");
-        //     document.write("<td><font color=" + color + ">" + resultTable[j + "단"].join("") + "</td>")
-        // }
-        // document.write("</tr>");
     }
     document.write("</table>");
     return resultTable;
