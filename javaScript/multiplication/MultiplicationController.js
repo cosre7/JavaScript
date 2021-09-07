9409a34098b9b79ee7a6e1237a67d51efc949213
cosre7 <76677629+cosre7@users.noreply.github.com>
2021-09-07 16:48:29 +0900

    리팩토링

--- javaScript/multiplication/MultiplicationController.js
+++ javaScript/multiplication/MultiplicationController.js
@@ -5,14 +5,15 @@ var MultiplicationController = {};
 */
 MultiplicationController.getTable = function(startRowNumber, endRowNumber, endColumNumber) {
     var startColumNumber = 1;
-    var startRowNumber = document.getElementById("startRowNumber").value;
-    var endRowNumber = document.getElementById("endRowNumber").value;
-    var endColumNumber = document.getElementById("endColumNumber").value;
+    var startRowNumber = Number(document.getElementById("startRowNumber").value);
+    var endRowNumber = Number(document.getElementById("endRowNumber").value);
+    var endColumNumber = Number(document.getElementById("endColumNumber").value);
+    
+    MultiplicationFactory.createMultiplyTable(startRowNumber,endRowNumber, startColumNumber, endColumNumber);
     if (startRowNumber > endRowNumber) {
         alert("첫 번째 단이 마지막 단보다 더 작아야 합니다.");
         history.go(0);
     } else {
-        var createTable = MultiplicationFactory.createMultiplyTable(startColumNumber , endColumNumber , startRowNumber , endRowNumber);
-        MultiplicationUI.printMultiplicationTable(createTable,startRowNumber, endRowNumber, endColumNumber);
+        MultiplicationUI.printMultiplicationTable(startRowNumber , endRowNumber, startColumNumber , endColumNumber);
     }
 }
