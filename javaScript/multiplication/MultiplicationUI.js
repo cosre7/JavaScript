e9e146c7124d44caf2a167f7e3ff8a6df79221f7
cosre7 <76677629+cosre7@users.noreply.github.com>
2021-09-07 16:47:21 +0900

    테이블 형태 출력

--- javaScript/multiplication/MultiplicationUI.js
+++ javaScript/multiplication/MultiplicationUI.js
@@ -2,20 +2,26 @@ var MultiplicationUI = {};
 /*
     이름 : printMultiplicationTable
     역할 : 곱셈 테이블을 출력한다.
-*/
-MultiplicationUI.printMultiplicationTable = function(table,startRowNumber, endRowNumber) {
-    // console.log(startRowNumber);
-    var resultTable = table;
-    document.write('<table border="1">');
+    재료
 
+    table[1] = [1*1=1], [1*2=2], 
+    table[2] = [2*1=1], [2*2=4],
+*/
+MultiplicationUI.printMultiplicationTable = function(startRowNumber , endRowNumber, startColumNumber , endColumNumber) {
+    document.write("<table border = 1>");
     document.write("<tr>");
-    for (i = startRowNumber; i <= endRowNumber; i++) {
-        for (j = startRowNumber; j <= i; j++) {
-            var color = (j % 2 == 1) ? "<font color=blue>" : "<font color=orange>";
-            document.write("<td>" + color + j + "단 <br/>" + resultTable[j + "단"].join("") + "</td>")
+    for (k = startRowNumber; k <= endRowNumber; k++) {
+        for (i = startRowNumber; i <= k; i++) {
+            document.write("<td>");
+            for (j = startColumNumber; j <= endColumNumber; j++) {
+                var color = (i % 2 == 1) ? "<font color=blue>" : "<font color=orange>";
+                var resultText = MultiplicationFactory.findMultiplyItem(i, j);
+                // resultText는 하나의 아이템
+                document.write(color + resultText + "<br/>");
+            }
+            document.write("</td>");
         }
         document.write("</tr>");
     }
     document.write("</table>");
-    return resultTable;
 }
